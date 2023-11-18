3b4dcf2af7eff579fee3e2fe1e67de3252f6cbda
taoguangc <taoguangc@gmail.com>
2023-11-18 19:56:35 +0800

    add layouts and components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,ts,tsx,js,jsx,md,mdx,svelte,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
 }
