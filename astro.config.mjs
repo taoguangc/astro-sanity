6cbe70314892e37038985268b9c51de370af3c96
taoguangc <taoguangc@gmail.com>
2023-11-18 16:30:03 +0800

    add tailwindcsss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import { sanityIntegration as sanity } from '@sanity/astro'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind(),
+    sanity({
+      projectId: 'k7xaxt1p',
+      dataset: 'production',
+      apiVersion: '2023-11-18',
+      useCdn: false
+    })
+  ]
+})
